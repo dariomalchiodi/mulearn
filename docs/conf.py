8681b7d78e7ca4578fda074b902b95fddd4ce124
Dario Malchiodi <malchiodi@di.unimi.it>
2020-12-22 09:55:39 +0100

    Added .readthedocs.yml

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ autodoc_member_order = 'bysource'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-'sphinx.ext.autodoc',
+    'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
