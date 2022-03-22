3a9649e065c7a7bed326f5a66e45fbf0fb09d840
Dario Malchiodi <malchiodi@di.unimi.it>
2022-03-22 09:38:31 +0100

    added first version of lagrange relaxation for tensorflow-based optimization

--- mulearn/optimization.py
+++ mulearn/optimization.py
@@ -270,18 +270,228 @@ class TensorFlowSolver(Solver):
           defaults to `tqdm.trange` if tqdm is available, to `range` (i.e., no
           graphical tool) otherwise.
         :type tracker: `object`
-        :param penalization: Lagrange penalization for the equality constraint
-          in the original problem, defaults to 10.
-        :type penalization: `float`
         """
+
         self.init_bound = init_bound
         self.initial_values = initial_values
         self.n_iter = n_iter
         self.optimizer = optimizer
         self.tracker = tracker
-        self.penalization = penalization
+
+    def solve_lagrange_relaxation(self, Q, q, A, b, C, d,
+                  max_iter=1000,
+                  max_gap=10**-2,
+                  alpha_0 = 10**-1,
+                  window_width = 30,
+                  verbose=False):
+        """Solves the lagrangian relaxation for a constrained optimization
+        problem and returns its result. The structure of the primal problem
+        is the following
+        
+        min x.T Q x + q.T x
+        subject to
+        A x = b
+        C x <= d
+        
+        where .T denotes the transposition operator. Optimization takes place
+        in a iterated two-steps procedure: an outer process devoted to modifying
+        the values of the lagrange multipliers, and an inner process working on
+        the primal variables.
+        
+        The arguments are as follows, given n as the number of variables of
+        the primal problem (i.e., the length of x)
+        
+        - Q: n x n matrix containing the quadratic coefficients of the cost
+        function;
+        - q: vector containing the n linear coefficients of the cost function
+        - A: s x n matrix containing the coefficients of the = constraints
+        - b: vector containing the s right members of the = constraints
+        - C: t x n matrix containing the coefficients of the <= constraings
+        - d: vector containing the t right members of the <= coefficients
+        - max_iter: maximum number of iterations of the *outer* optimization
+        procedure
+        - max_gap: maximum gap between primal and dual objectives ensuring
+        premature end of the *outer* optimization procedure
+        - alpha_0: initial value of the learning rate in the *outer* optimization
+        procedure
+        - window_width: width of the moving window on the objective function for
+        the *inner* optimization process
+        - verbose: boolean flag triggering verbose output
+        
+        returns
+        """
+
+        #TODO add possibility to specify initial values
+
+        x = tf.Variable(np.random.random(len(q)),
+                        name='x',
+                        trainable=True,
+                        dtype=tf.float32)
+        Q = tf.constant(np.array(Q), dtype='float32')
+        q = tf.constant(np.array(q), dtype='float32')
+        
+        A = np.array(A)
+        s = len(A)
+        C = np.array(C)
+        b = np.array(b)
+        d = np.array(d)
+        
+        M = np.vstack([A, -A, C])
+        m = np.hstack([b, -b, d])
+        lambda_ = tf.constant(np.random.random(len(m)), dtype='float32')
+        
+        M = tf.constant(M, dtype='float32')
+        m = tf.constant(m, dtype='float32')
+
+        
+        def original_objective():
+            def obj():
+                return tf.tensordot(tf.linalg.matvec(Q, x), x, axes=1) + \
+                    tf.tensordot(q, x, axes=1)
+            return obj
+
+        def lagrangian_objective(lambda_):
+            def obj():
+                return tf.tensordot(tf.linalg.matvec(Q, x), x, axes=1) + \
+                    tf.tensordot(q, x, axes=1) + \
+                    tf.tensordot(lambda_, m - tf.linalg.matvec(M, x), axes=1)
+            return obj
+
+        obj_val = []
+        lagr_val = []
+        gap_val = []
+        gap = max_gap + 1
+
+        num_bad_iterations = 0
+        prev_orig = np.inf
+
+        i = 0
+        
+        while i < max_iter and (gap<0 or gap > max_gap):
+            lagr_obj = lagrangian_objective(lambda_)
+            orig_obj = original_objective()
+            prev_lagr = 10*3
+            curr_lagr = 0
+            vals = []
+            t = 0
+            window_width = 30
+            window = list(np.logspace(1, window_width, window_width))
+            # this is to ensure a high value for the standard deviation
+            # of the elements to which the window has been initialized 
+            
+            while (np.std(window)/abs(np.mean(window)) > 0.001 or t < 100) \
+                and t < 1000:
+                self.optimizer.minimize(lagr_obj, var_list=x)
+                prev_lagr = curr_lagr
+                curr_lagr = lagr_obj().numpy()
+                vals.append(curr_lagr)
+                t += 1
+                window = window[1:]
+                window.append(curr_lagr)
+            
+            curr_orig = orig_obj().numpy()
+            if curr_orig < prev_orig:
+                num_bad_iterations += 1
+
+            prev_orig = curr_orig
+
+            
+            obj_val.append(curr_orig)
+            lagr_val.append(curr_lagr)
+            
+            subgradient = (m - tf.linalg.matvec(M, x)).numpy()
+            gap = tf.tensordot(lambda_[:2*s], m[:2*s] - \
+                tf.linalg.matvec(M[:2*s], x), axes=1).numpy()
+            gap_val.append(gap)
+            
+            if verbose and i%1 == 0:
+                print(f'i={i}, dual={lagr_obj().numpy():.3f}, '
+                    f'prim={orig_obj().numpy():.3f}, '
+                    f'gap={gap:.6f}')
+            
+            alpha = alpha_0 / num_bad_iterations
+            lambda_ = tf.maximum(0, lambda_ + alpha * subgradient)
+
+            i += 1
+
+        return obj_val, lagr_val, x, lambda_, gap_val
 
     def solve_problem(self, xs, mus, c, k):
+        if not tensorflow_ok:
+            raise ValueError('tensorflow not available')
+
+        m = len(xs)
+
+        if self.initial_values == 'random':
+            alphas = [tf.Variable(ch, name=f'alpha_{i}',
+                                  trainable=True, dtype=tf.float32)
+                      for i, ch in enumerate(np.random.uniform( \
+                                -self.init_bound, self.init_bound, m))]
+            betas = [tf.Variable(ch, name=f'beta_{i}',
+                                  trainable=True, dtype=tf.float32)
+                      for i, ch in enumerate(np.random.uniform( \
+                                -self.init_bound, self.init_bound, m))]
+
+        elif isinstance(self.initial_values, Iterable):
+            alphas = [tf.Variable(ch, name=f'alpha_{i}',
+                                trainable=True, dtype=tf.float32)
+                    for i, ch in enumerate(self.initial_values)]
+            betas = [tf.Variable(ch, name=f'beta_{i}',
+                                trainable=True, dtype=tf.float32)
+                    for i, ch in enumerate(self.initial_values)]
+
+        else:
+            raise ValueError("`initial_values` should either be set to "
+                             "'random' or to a list of initial values.")
+        
+        if type(k) is kernel.PrecomputedKernel:
+            gram = k.kernel_computations
+        else:
+            gram = np.array([[k.compute(x1, x2) for x2 in xs] for x1 in xs])
+        
+        x = alphas + betas
+
+        K11 = np.array([[-mu_i * mu_j for mu_j in mus] for mu_i in mus]) * gram
+        K00 = np.array([[-(1-mu_i) * (1 - mu_j) for mu_j in mus]
+                         for mu_i in mus]) * gram
+        K01 = np.array([[2 * mu_i * (1-mu_j) for mu_j in mus]
+                         for mu_i in mus]) * gram
+        Z = np.zeros((m, m))
+
+        Q = -np.vstack((np.hstack((K11, K01)), np.hstack((Z, K00))))
+        q = -np.hstack((np.diag(gram) * mus, np.diag(gram) * (1 - mus)))
+
+        A = np.array([np.hstack((mus, 1-mus))])
+        b = np.array([1])
+
+        C = np.vstack((np.hstack((np.identity(m), np.zeros((m, m)))),
+                       np.hstack((np.zeros((m, m)), -np.identity(m)))))
+        d = np.hstack((np.zeros(m), - c * np.ones(m)))
+
+        x = np.random.random(2*m)
+        assert(Q.shape == (2*m, 2*m))
+        assert(len(q) == 2*m)
+        assert(type(x @ (Q @ x) + q @ x) is np.float64)
+
+        assert(A.shape == (1, 2*m))
+        assert(len(b) == 1)
+        assert(type(A@x - b) == np.ndarray)
+        assert(len(A@x - b) == 1)
+
+        assert(C.shape == (2*m, 2*m))
+        assert(len(d) == 2*m)
+        assert(type(C @ x - d) == np.ndarray)
+        assert(len(C @ x - d) == 2*m)
+
+        _, _, x, _, _ = self.solve_lagrange_relaxation(Q, q, A, b, C, d, verbose=True)
+
+        alphas = np.array(x[:m])
+        betas = np.array(x[m:])
+
+        return alphas * np.array(mus) - betas * (1 - np.array(mus))
+
+
+    def solve_problem_old(self, xs, mus, c, k):
         """Optimize via TensorFlow.
 
         Build and solve the constrained optimization problem on the basis
@@ -352,7 +562,7 @@ class TensorFlowSolver(Solver):
         obj_repr = f"TensorFlowSolver("
 
         for a in ("initial_values", "init_bound", "n_iter",
-                  "optimizer", "tracker", "penalization"):
+                  "optimizer", "tracker"):
             if self.__getattribute__(a) != self.default_values[a]:
                 obj_repr += f", {a}={self.default_values[a]}"
         return obj_repr + ")"
